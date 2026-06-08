4b20a44e62aa197300cf3a5fa48fbcdd24c19cc6
info1127 <info.dusc@daffodilvarsity.edu.bd>
2026-06-08 11:46:16 +0600

    Update admit_app.py

--- admit_app.py
+++ admit_app.py
@@ -73,7 +73,7 @@ class AdmitCardPDF(FPDF):
 
         self.set_x(inner_x + 5)
         self.cell(
-            40,
+            -40,
             8,
     "Accounts Signature: __________                         Principal Signature: __________",
             align="C",
